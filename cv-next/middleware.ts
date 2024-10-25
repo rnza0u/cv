4116a9578185b222d49152822283acad23f8b579
rnzaou <nzaou.renaud@gmail.com>
2024-10-25 13:18:05 +0200

    wip

--- cv-next/middleware.ts
+++ cv-next/middleware.ts
@@ -1,21 +1,42 @@
 import { MiddlewareConfig, NextRequest, NextResponse } from 'next/server'
 import { match } from '@formatjs/intl-localematcher'
 import Negotiator from 'negotiator'
-import { locales, defaultLocale } from './src/translations/locales'
+import { locales, defaultLocale, Locale } from './src/translations/locales'
 
-export function middleware(request: NextRequest) {
+const LOCALE_COOKIE_NAME = 'CV_LOCALE'
+
+export async function middleware(request: NextRequest) {
+
+    console.log((request as any).headers)
+
     const { pathname } = request.nextUrl
-    const hasLocaleInPath = locales.some(locale => pathname === `/${locale}` || pathname.startsWith(`/${locale}/`))
+    const pathLocale = locales.find(locale => pathname === `/${locale}` || pathname.startsWith(`/${locale}/`))
 
-    if (hasLocaleInPath)
+    if (pathLocale) {
+        const existingCookie = request.cookies.get(LOCALE_COOKIE_NAME)
+        if (existingCookie?.value !== pathLocale){
+            const response = NextResponse.next()
+            response.cookies.set(LOCALE_COOKIE_NAME, pathLocale, {
+                httpOnly: true,
+                sameSite: 'lax',
+                maxAge: 3600 * 60 * 24 * 7,
+                secure: process.env.NODE_ENV !== 'development'
+            })
+        }
         return
+    }
     
-    const userLocales = new Negotiator({ 
-        headers: Object.fromEntries(Array.from(request.headers.entries())) 
-    }).languages()
-    const locale = match(userLocales, locales, defaultLocale)
+    
+    const locale = request.cookies.get(LOCALE_COOKIE_NAME) ?? inferLocale(request)
     request.nextUrl.pathname = `/${locale}${pathname}`
     return NextResponse.rewrite(request.nextUrl)
+}
+
+function inferLocale(request: NextRequest): Locale {
+    const userLocales = new Negotiator({
+        headers: Object.fromEntries(request.headers.entries()) 
+    }).languages()
+    return match(userLocales, locales, defaultLocale) as Locale
 }
 
 export const config: MiddlewareConfig = {
